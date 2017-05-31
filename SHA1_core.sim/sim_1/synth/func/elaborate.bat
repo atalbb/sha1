c7a2032288397727ed7def103adb419be32b3b46
Atal <atal.bajracharya@gmailcom>
2017-05-31 16:05:17 -0400

    Shifting logic in wt[16] corrected

---
+++ SHA1_core.sim/sim_1/synth/func/elaborate.bat
@@ -0,0 +1,9 @@
+@echo off
+set xv_path=C:\\Xilinx\\Vivado\\2016.3\\bin
+call %xv_path%/xelab  -wto dd07ee5cfe8046548a813284afb3a1c9 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L secureip --snapshot SHA1_core_tb_func_synth xil_defaultlib.SHA1_core_tb xil_defaultlib.glbl -log elaborate.log
+if "%errorlevel%"=="0" goto SUCCESS
+if "%errorlevel%"=="1" goto END
+:END
+exit 1
+:SUCCESS
+exit 0
