0f66c80bd28b31c88ea2b20d3ebcf16660fa410d
Atal <atal.bajracharya@gmailcom>
2017-05-31 13:52:46 -0400

    1st commit. 1 round of SHA1 done

---
+++ SHA1_core.srcs/sim_1/new/SHA1_core_tb.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/30/2017 01:38:40 PM
+// Design Name: 
+// Module Name: SHA1_core_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SHA1_core_tb(
+
+    );
+reg clk,rst;
+reg [511:0]blk;
+wire done;
+wire [159:0]digest;
+SHA1_core S1(clk,rst,blk,done,digest);
+initial begin
+    clk = 0;
+    #5 rst = 0;
+    #10 rst = 1;
+    #6000 $finish;
+end
+always begin
+    #5 clk = ~clk;
+end
+endmodule
